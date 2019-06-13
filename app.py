daeb6b98141cb193873ba23161a42ad2e189ed46
bilin.wang <wangbilin@orientsoft.cn>
2019-06-13 15:49:22 +0800

    全局捕获异常

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 import yaml
+import logging
+import traceback
 
 app = Flask(__name__)
 # 加载配置文件
@@ -12,3 +14,9 @@ with open('config.yaml', 'r', encoding='utf-8') as f:
 from applications.api import api
 
 app.register_blueprint(api)
+
+
+@app.errorhandler(Exception)
+def error_handler(error):
+    logging.error('Request Error: {}\nStack: {}\n'.format(error, traceback.format_exc()))
+    return 'matplot-service 未知错误', 500
